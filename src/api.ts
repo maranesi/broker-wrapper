559d22fa837fb35f7aa0c300f7d0d076260510ad
Kenneth Freitas <kennethmf@outlook.com>
2020-11-11 17:52:25 -0300

    improvements

--- src/api.ts
+++
@@ -1,8 +0,0 @@
-import { getBroker } from './broker';
-
-const run = async () => {
-  const broker = await getBroker();
-  await broker.send("USER_DELETED", {id:100, name:"Bruno Maranesi"});
-}
-
-run().catch(console.error);
